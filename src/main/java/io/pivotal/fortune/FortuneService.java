c701cb8825f88495fbb3c7c7f55e21bbc5d1e247
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-02-28 16:57:49 -0500

    Debugging

--- src/main/java/io/pivotal/fortune/FortuneService.java
+++ src/main/java/io/pivotal/fortune/FortuneService.java
@@ -3,9 +3,14 @@ package io.pivotal.fortune;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Component
 public class FortuneService {
@@ -22,10 +27,22 @@ public class FortuneService {
     this.restTemplate = restTemplate;
   }
 
+  @Autowired
+  private LoadBalancerClient loadBalancer;
+
   @HystrixCommand(fallbackMethod = "defaultFortune")
   public String getFortune() {
     logger.debug("Using fortuneServiceURL=[{}]", fortuneServiceURL);
-    String fortune = restTemplate.getForObject(fortuneServiceURL, String.class);
+
+      ServiceInstance instance = loadBalancer.choose("fortune-service");
+      URI secondServiceUri = URI.create(String.format("http://%s:%s", instance.getHost(), instance.getPort()));
+
+//      System.out.println(secondServiceUri); // logs http://192.168.0.205:8090, check log below
+      logger.debug(secondServiceUri.toString());
+
+      RestTemplate rt = new RestTemplate();
+
+    String fortune = rt.getForObject("https://" + instance.getHost(), String.class);
     logger.debug("Got fortune=[{}]", fortune);
     return fortune;
   }
